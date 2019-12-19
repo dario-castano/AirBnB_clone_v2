c3dba2255c8b6aa35c0712b61300dc6c13ddcb36
dalexach <dalexach@gmail.com>
2019-12-19 20:04:58 +0000

    correction on the console

--- tests/test_console.py
+++ tests/test_console.py
@@ -134,7 +134,7 @@ class TestConsole(unittest.TestCase):
             self.assertEqual("** class doesn't exist **\n", f.getvalue())
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("all State")
-            self.assertEqual("[]\n", f.getvalue())
+            self.assertEqual("[]\n", str(f.getvalue()))
 
     def test_update(self):
         """Test update command inpout"""
