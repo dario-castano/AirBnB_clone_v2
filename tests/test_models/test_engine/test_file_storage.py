80a70b0b78227e89516a74d980890217a363bbca
Dario Castaño <dario.castano@aim.com>
2019-12-19 22:49:09 -0500

    Skip FileStorage tests in DB mode

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,7 +13,7 @@ from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
 
-
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "Won't work in DB")
 class TestFileStorage(unittest.TestCase):
     '''this will test the FileStorage'''
 
