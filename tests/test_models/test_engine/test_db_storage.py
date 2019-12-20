11855c127afdb6486b4cb4009d464b24dab5de87
Dario Castaño <dario.castano@aim.com>
2019-12-19 21:54:11 -0500

    WIP: Check mysqldb connection

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,6 +4,8 @@ import unittest
 import pep8
 import json
 import os
+import MySQLdb
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -14,9 +16,8 @@ from models.review import Review
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     "Won't work in DB")
 
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "Won't work in DB")
 class TestDBStorage(unittest.TestCase):
     '''this will test the DBStorage'''
 
@@ -77,3 +78,44 @@ class TestDBStorage(unittest.TestCase):
         """
         self.session.close()
         self.session.rollback()
+
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db' and
+    os.getenv('HBNB_ENV') == 'test', "HBNB_ENV=test not set")
+class TestDBStorage2(unittest.TestCase):
+    """
+    Tests for DBStorage inside the DB
+    """
+    @classmethod
+    def setUpClass(cls):
+        db_conf = {
+            'host': os.getenv('HBNB_MYSQL_HOST'),
+            'port': 3306,
+            'user': os.getenv('HBNB_MYSQL_USER'),
+            'passwd': os.getenv('HBNB_MYSQL_PWD'),
+            'db': os.getenv('HBNB_MYSQL_DB'),
+        }
+        print(db_conf)
+        cls.store = DBStorage()
+        cls.conn = MySQLdb.connect(**db_conf)
+
+    def tearDownClass(self):
+        self.conn.close()
+
+    def setUp(self):
+        self.store.reload()
+        self.cursor = self.conn.cursor()
+
+    def tearDown(self):
+        self.cursor.close()
+
+    def test_State_create(self):
+        """DBStorage are writing States in the DB?
+        """
+        name = 'Arizona'
+        new_state = State(eval('name="{}"'.format(name)))
+        self.store.new(new_state)
+        self.store.save()
+        self.cursor.execute('SELECT * FROM states WHERE name=%s', (name,))
+        out = self.cursor.fetchall()
+        self.assertGreater(len(out), 0)
+
