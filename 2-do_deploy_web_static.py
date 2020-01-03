f7c290e9d48816c0874c5bb9401a17f950772ce2
Dario Castaño <dario.castano@aim.com>
2020-01-03 17:56:55 -0500

    Add try block to do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,15 +34,18 @@ def do_deploy(archive_path):
         untar_path = "/data/web_static/releases/{}"\
             .format(tar_name.replace('.tgz', ''))
 
-        put(archive_path, '/tmp')
-        run("mkdir -p {}".format(untar_path))
-        run("tar -zxf /tmp/{} -C {}".format(tar_name, untar_path))
-        run("rm /tmp/{}".format(tar_name))
-        run("mv {}/web_static/* {}".format(untar_path, untar_path))
-        run("rm -rf {}/web_static".format(untar_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(untar_path))
-        print('New version deployed!')
-        return True
+        try:
+            put(archive_path, '/tmp')
+            run("mkdir -p {}".format(untar_path))
+            run("tar -zxf /tmp/{} -C {}".format(tar_name, untar_path))
+            run("rm /tmp/{}".format(tar_name))
+            run("mv {}/web_static/* {}".format(untar_path, untar_path))
+            run("rm -rf {}/web_static".format(untar_path))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {} /data/web_static/current".format(untar_path))
+            print('New version deployed!')
+            return True
+        except Exception:
+            return False
     else:
         return False
