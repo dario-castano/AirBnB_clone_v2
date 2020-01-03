7198fd2f7230c28664d78cd31303a63f24bf4ce0
Dario Castaño <dario.castano@aim.com>
2020-01-03 17:52:00 -0500

    Add return True to if else block

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -43,5 +43,6 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(untar_path))
         print('New version deployed!')
+        return True
     else:
         return False
