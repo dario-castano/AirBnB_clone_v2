4eae71ca2768becc7f43b0cf3cc7702d1b731e94
Dario Castaño <dario.castano@aim.com>
2020-01-04 13:13:29 -0500

    Add default nginx config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,7 +21,7 @@ echo "
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/server_name _;/a \ \n$LOCATION" /etc/nginx/sites-available/default
+sudo wget https://github.com/dario-castano/AirBnB_clone_v2/blob/master/default -O /etc/nginx/sites-available/default
 sudo service nginx restart
