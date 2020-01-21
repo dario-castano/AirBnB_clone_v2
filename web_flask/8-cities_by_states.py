09654a0e3022e8f7693f05d818225fb73e51040c
Dario Castaño <dario.castano@aim.com>
2020-01-21 11:09:19 -0500

    Add task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Hello flask module
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """
+    List cities and states in a jinja template
+    """
+    states = storage.all("State")
+    cities = storage.all("City")
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """
+    Closes the storage
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
